806a43b9ef9c8897a798ab28a0a758b18df4cf1a
Sohan <mdsohan383@gmail.com>
2024-02-18 18:35:38 +0600

    routes abstracted

--- index.ts
+++ index.ts
@@ -1,15 +1,14 @@
 import express, { Request, Response, NextFunction, response } from 'express';
 import dotenv, { config } from 'dotenv'; // For secure environment variable handling
-import { Order } from './src/Utils/orderType';
 import { PrismaClient } from '@prisma/client';
 import validatedShopifySignature from './src/middlewares/Verify';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import rateLimit from 'express-rate-limit';
 import CustomError from './src/Utils/CustomError';
-import createOrder from './src/functions/createOrder';
-import updateOrder from './src/functions/updateOrder';
+
 const errorHandler  = require("./src/controllers/errorHandler")
-import { param, query, validationResult } from 'express-validator';
+import { param } from 'express-validator';
+import updateOrderHandler from './src/routes/updateRoute';
+import upsertRoute from './src/routes/upsertRoute';
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -43,78 +42,29 @@ const validateParams = [
   param('id').notEmpty().isInt().toInt(), // Ensure 'id' is not empty and is an integer
   // Add more validation rules as needed for other parameters
 ];
-app.put("/webhooks/orders/update/:id",validateParams, validatedShopifySignature(), async(req : Request, res : Response, next : NextFunction) => {
-  const order: Order = req.body
-//check if the order is coming from valid app id, can set to (order.app_id !== "specific shopify app id")
-  const errors  = validationResult(req)
-  console.log(`errors are in validation ${JSON.stringify(errors)}`)
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-  if (!order.app_id) {
-    const err = new CustomError("no valid app id", 401)
-    next(err)
-  }
-  const {id} = req.params
-  console.log(id)
-  // save the order data in database and check for error type
-  const [data, error] = await updateOrder(parseInt(id), order)
-  if(data) {
-    res.status(200).json({
-      status : "success",
-      data : data
-     }) 
-  }
-  if (error) {
-    if ( error instanceof PrismaClientKnownRequestError) {
-    const err = new CustomError(error.message, 404)
-      next(err)
-    }
+app.put("/webhooks/orders/update/:id",
+validateParams, 
+validatedShopifySignature(), 
+updateOrderHandler
+)
 
-    next(error)
-  } 
- 
-  console.log("Webhook received for update")
-  
-} )
+
 //order creation route
 app.post('/webhooks/orders/create',
   // validate the request using middleware
   validatedShopifySignature(),
-  async (req: Request, res: Response, next : NextFunction) => {
-    const order: Order = req.body
-    console.log(`order is ${order.id}`)
-  //check if the order is coming from valid app id, can set to (order.app_id !== "specific shopify app id")
+  upsertRoute
 
-    if (!order.app_id) {
-      const err = new CustomError("no valid app id", 401)
-      next(err)
-    }
-    // save the order data in database and check for error type
-    const [data, error] = await createOrder(order)
-    if(data) {
-      res.status(200).json({
-        status : "success",
-        data
-       }) 
-    }
-    if (error) {
-      if ( error instanceof PrismaClientKnownRequestError) {
-      const err = new CustomError(error.message, 404)
-        next(err)
-      }
+  );
 
-      next(error)
-    } 
-   
-    console.log("Webhook received")
-    
-  });
+  
   //error handle all unauthorised routes
   app.all('*', (req : Request, res : Response, next : NextFunction) => {
     const err = new CustomError(`can't find ${req.originalUrl} on the server`, 404) 
        next(err)
   })
+
+
 //global error handling middleware
   app.use(errorHandler)
 
